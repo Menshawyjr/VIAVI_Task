550e95f109f6b1c96d04339a38caf81a68a71668
Ahmed Menshawy <Ahmed0menshawy@hotmail.com>
2025-10-07 22:51:23 +0300

    new project

---
+++ src/test/java/config/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    public static final String BASE_URL = "https://demo.prestashop.com/";
+    public static final int TIMEOUT = 10;
+    public static final boolean HEADLESS = false;
+    public static final String BROWSER = "chrome"; // chrome, firefox, edge
+}
